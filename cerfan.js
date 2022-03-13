de7caea0509312dc16a27cad512075d57dd7abd0
makarskiioleg61 <89338865+makarskiioleg61@users.noreply.github.com>
2022-03-13 15:50:40 +0200

    Create cerfan.js

---
+++ cerfan.js
@@ -0,0 +1,19 @@
+</p>
+<br/>
+
+- [Chainlink Hardhat Box](#chainlink-hardhat-box)
+- [Chainlink Hardhat Starter Kit](#chainlink-hardhat-starter-kit)
+- [Getting Started](#getting-started)
+  - [Requirements](#requirements)
+  - [Quickstart](#quickstart)
+    - [Typescript](#typescript)
+- [Useage](#useage)
+- [Usage](#usage)
+  - [Deploying Contracts](#deploying-contracts)
+  - [Run a Local Network](#run-a-local-network)
+  - [Using a Testnet or Live Network (like Mainnet or Polygon)](#using-a-testnet-or-live-network-like-mainnet-or-polygon)
+    - [Kovan Ethereum Testnet Setup](#kovan-ethereum-testnet-setup)
+    - [Rinkeby Ethereum Testnet Setup](#rinkeby-ethereum-testnet-setup)
+  - [Forking](#forking)
+  - [Auto-Funding](#auto-funding)
+- [Test](#test)
